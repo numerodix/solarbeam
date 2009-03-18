27fac142290c1ee789157b1245870f93a4004484
Martin Matusiak <numerodix@gmail.com>
2009-03-19 00:33:51 +0100

    locationlist

--- solarbeam/GuiDatasource/LocationsSource.cs
+++ solarbeam/GuiDatasource/LocationsSource.cs
@@ -24,12 +24,14 @@ namespace SolarbeamGui
 				list = GetDummyList();
 			}
 
-			locations_array = new string[list.Count];
-			int i=-1;
+			// sort keys
+			List<string> keylist = new List<string>();
 			foreach (string name in list.Keys) {
-				i++;
-				locations_array[i] = list.Get(name).Name;
+				keylist.Add(name);
 			}
+			keylist.Sort();
+			
+			locations_array = keylist.ToArray();
 		}
 		
 		// serialize list in finalizer
@@ -51,27 +53,12 @@ namespace SolarbeamGui
 		private LocationList GetDummyList()
 		{
 			list = new LocationList();
-			list.Add("Baku", "Asia/Baku",
-					new Position(Position.LATITUDE_POS, 40, 23, 43,
-						Position.LONGITUDE_POS, 49, 52, 56));
-			list.Add("Buenos Aires", "America/Buenos_Aires",
-					new Position(Position.LATITUDE_NEG, 34, 36, 36,
-						Position.LONGITUDE_NEG, 58, 22, 12));
 			list.Add("Equator", "UTC",
-					new Position(Position.LATITUDE_NEG, 0, 0, 0,
-						Position.LONGITUDE_NEG, 0, 0, 0));
-			list.Add("Sydney", "Australia/Sydney",
-				new Position(Position.LATITUDE_NEG, 33, 51, 36,
-						Position.LONGITUDE_POS, 151, 12, 40));
-			list.Add("Tromsø", "Europe/Oslo",
-					new Position(Position.LATITUDE_POS, 69, 40, 58,
-						Position.LONGITUDE_POS, 18, 56, 34));
-			list.Add("Trondheim", "Europe/Oslo",
-					new Position(Position.LATITUDE_POS, 63, 25, 47,
-						Position.LONGITUDE_POS, 10, 23, 36));
-			list.Add("Utrecht", "Europe/Amsterdam",
-					new Position(Position.LATITUDE_POS, 52, 5, 36,
-						Position.LONGITUDE_POS, 5, 7, 10));
+				new Position(Position.LATITUDE_NEG, 0, 0, 0,
+			        Position.LONGITUDE_NEG, 0, 0, 0));
+			
+			
+			
 			return list;
 		}
 		
