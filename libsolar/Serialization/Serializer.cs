762873bda4c67033ef4859afd239fad454bb4be3
Martin Matusiak <numerodix@gmail.com>
2009-03-18 21:29:14 +0100

    serializer

---
+++ libsolar/Serialization/Serializer.cs
@@ -0,0 +1,30 @@
+// Copyright (c) 2009 Martin Matusiak <numerodix@gmail.com>
+// Licensed under the GNU Public License, version 3.
+
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace LibSolar.Serialization
+{
+	public class Serializer
+	{
+		public static void Serialize(string path, object obj)
+		{
+			using (Stream stream = File.OpenWrite(path)) {
+				BinaryFormatter formatter = new BinaryFormatter();
+				formatter.Serialize(stream, obj);
+			}
+		}
+		
+		public static object Deserialize(string path)
+		{
+			object obj = null;
+			using (Stream stream = File.OpenRead(path)) {
+				BinaryFormatter formatter = new BinaryFormatter();
+				obj = formatter.Deserialize(stream);
+			}
+			return obj;
+		}
+	}
+}
