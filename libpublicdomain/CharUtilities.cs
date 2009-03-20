3a089d2c59bc96b4a48e74e7393a4b4e9eb058a7
Martin Matusiak <numerodix@gmail.com>
2009-03-20 23:50:45 +0100

    dead

--- libpublicdomain/CharUtilities.cs
+++ libpublicdomain/CharUtilities.cs
@@ -10,33 +10,6 @@ namespace PublicDomain
     public class CharUtilities
     {
         /// <summary>
-        /// 
-        /// </summary>
-        public static readonly char[] AsciiCharacters;
-
-        /// <summary>
-        /// Initializes the <see cref="CharUtilities"/> class.
-        /// </summary>
-        static CharUtilities()
-        {
-            AsciiCharacters = GetAsciiCharacters().ToArray();
-        }
-
-        /// <summary>
-        /// Gets the ASCII characters.
-        /// </summary>
-        /// <returns></returns>
-        public static List<char> GetAsciiCharacters()
-        {
-            List<char> result = new List<char>(256);
-            for (int i = 0; i < 256; i++)
-            {
-                result.Add((char)i);
-            }
-            return result;
-        }
-
-        /// <summary>
         /// Determines whether [is character one of] [the specified c].
         /// </summary>
         /// <param name="c">The c.</param>
@@ -59,16 +32,5 @@ namespace PublicDomain
             return false;
         }
 
-        /// <summary>
-        /// Determines whether [is quote character] [the specified c].
-        /// </summary>
-        /// <param name="c">The c.</param>
-        /// <returns>
-        /// 	<c>true</c> if [is quote character] [the specified c]; otherwise, <c>false</c>.
-        /// </returns>
-        public static bool IsQuoteCharacter(char c)
-        {
-            return c == '\"' || c == '\'' || c == '“' || c == '”';
-        }
     }
 }
