af720b29fb15925b74400445b120552c818e2706
Martin Matusiak <numerodix@gmail.com>
2009-04-01 00:36:56 +0200

    use Repeat attribute

--- libsolar/SolarOrbit/Test/CoordinateTest.cs
+++ libsolar/SolarOrbit/Test/CoordinateTest.cs
@@ -16,40 +16,36 @@ namespace LibSolar.SolarOrbit.Test
 		 * Test roundtrip conversion.
 		 */
 		[Test]
+		[Repeat(100)]
 		public void TestDegToRad()
 		{
-			for (int i = 0; i < 100; i++)
-			{
-				int deg = Rand.GetInt(-360, 360);
-				int new_deg = (int) 
-					Math.Round( Coordinate.RadToDeg( Coordinate.DegToRad(deg) ) );
-
-				Assert.AreEqual(deg, new_deg);
-			}
+			int deg = Rand.GetInt(-360, 360);
+			int new_deg = (int) 
+				Math.Round( Coordinate.RadToDeg( Coordinate.DegToRad(deg) ) );
+			
+			Assert.AreEqual(deg, new_deg);
 		}
 
 		/**
 		 * Test roundtrip conversion.
 		 */
 		[Test]
+		[Repeat(100)]
 		public void TestRadToDeg()
 		{
 			Random random = new Random();
-			for (int i = 0; i < 100; i++)
-			{
-				// number of digits to check for : 9
-				int scale = 1000000000;
-				double rad = (random.Next(-scale, scale+1) * Math.PI) / scale;
+			// number of digits to check for : 9
+			int scale = 1000000000;
+			double rad = (random.Next(-scale, scale+1) * Math.PI) / scale;
 
-				double new_rad =
-					Coordinate.DegToRad( Coordinate.RadToDeg(rad) );
+			double new_rad =
+				Coordinate.DegToRad( Coordinate.RadToDeg(rad) );
 
-//				Console.WriteLine("\nrad : " + rad);
-//				Console.WriteLine("new_rad : " + new_rad);
+//			Console.WriteLine("\nrad : " + rad);
+//			Console.WriteLine("new_rad : " + new_rad);
 
-				double diff = Math.Abs(rad - new_rad);
-				Assert.LessOrEqual(diff, (double) 1 / (double) scale);
-			}
+			double diff = Math.Abs(rad - new_rad);
+			Assert.LessOrEqual(diff, (double) 1 / (double) scale);
 		}
 	}
 }
--- libsolar/SolarOrbit/Test/JulianDateTest.cs
+++ libsolar/SolarOrbit/Test/JulianDateTest.cs
@@ -16,35 +16,31 @@ namespace LibSolar.SolarOrbit.Test
 		 * Test roundtrip conversion.
 		 */
 		[Test]
+		[Repeat(100)]
 		public void TestDayToCentury()
 		{
-			for (int i = 0; i < 100; i++)
-			{
-				int day = Rand.GetInt(1, 2455853); // 2009 + 1000
-				int new_day = (int) Math.Round( JulianDate.CalcJulianDay(
+			int day = Rand.GetInt(1, 2455853); // 2009 + 1000
+			int new_day = (int) Math.Round( JulianDate.CalcJulianDay(
 						JulianDate.CalcJulianCentury(day) ) );
 
-				Assert.AreEqual(day, new_day);
-			}
+			Assert.AreEqual(day, new_day);
 		}
 
 		/**
 		 * Test roundtrip conversion.
 		 */
 		[Test]
+		[Repeat(100)]
 		public void TestCenturyToDay()
 		{
 			Random random = new Random();
-			for (int i = 0; i < 100; i++)
-			{
-				int scale = 10000;
-				double cent = (double) random.Next(-scale, scale+1) / (double) scale;
-				double new_cent = JulianDate.CalcJulianCentury(
-						JulianDate.CalcJulianDay(cent) );
+			int scale = 10000;
+			double cent = (double) random.Next(-scale, scale+1) / (double) scale;
+			double new_cent = JulianDate.CalcJulianCentury(
+					JulianDate.CalcJulianDay(cent) );
 
-				double diff = Math.Abs(cent - new_cent);
-				Assert.LessOrEqual(diff, (double) 1 / (double) scale);
-			}
+			double diff = Math.Abs(cent - new_cent);
+			Assert.LessOrEqual(diff, (double) 1 / (double) scale);
 		}
 	}
 }
