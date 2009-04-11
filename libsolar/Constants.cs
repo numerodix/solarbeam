41efa1d11d57416eabb21ad28e432cf3279cc840
Martin Matusiak <numerodix@gmail.com>
2009-04-11 12:42:52 +0200

    extract asm info to single location

---
+++ libsolar/Constants.cs
@@ -0,0 +1,19 @@
+// Copyright (c) 2009 Martin Matusiak <numerodix@gmail.com>
+// Licensed under the GNU Public License, version 3.
+
+using System;
+
+namespace LibSolar
+{
+	public static class Constants
+	{
+		public const string LIBSOLAR_APPTITLE = "LibSolar";
+		public const string CONSOLE_APPTITLE = "SolarBeam Console";
+		public const string GUI_APPTITLE = "SolarBeam";
+		public const string GUI_APPDESC = "An application for drawing solar diagrams";
+
+		public const string VERSION = "1.0.0.0";
+
+		public const string COPYRIGHT = "Copyright (c) 2009 Martin Matusiak <numerodix@gmail.com>";
+	}
+}
