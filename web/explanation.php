a01ecea1c12698d0ece6453eb743b761d917abc5
Martin Matusiak <numerodix@gmail.com>
2009-04-12 18:50:21 +0200

    web

--- web/explanation.php
+++ web/explanation.php
@@ -26,6 +26,7 @@
 		<h2>Reading the diagram</h2>
 		<p>Suppose there is an observer somewhere on Earth. A solar diagram is then a chart of where the observer sees the sun in the sky, how high (elevation) and in what <a href="http://en.wikipedia.org/wiki/Cardinal_direction">direction</a> he has to look (azimuth).
 		</p>
+
 		<p>This diagram shows the sun's trajectory during some particular day of the year. Along the trajectory (the red line) there are particular points of interest:
 		<ul>
 			<li>The sun rises in the East. This is shown in the diagram by the fact that the angle of elevation exceeds 0°.</li>
@@ -34,6 +35,23 @@
 		</ul>
 		</p>
 		<img src="imgs/exp_diagram.png">
+		<p>Let us now look at a complete diagram.</p>
+
+		<h2>Case study: Copenhagen</h2>
+		<p>A complete diagram shows not only the trajectory for a single day, but also the trajectories for certain characteristic days of the year:</p>
+		<ul>
+			<li>June 21 is the summer <a href="http://en.wikipedia.org/wiki/Solstice">solstice</a> and the longest day of the year in the <a href="http://en.wikipedia.org/wiki/Northern_hemisphere">Northern Hemisphere</a> (shortest in the Southern).</li>
+			<li>December 21 is the winter solstice and the shortest day of the year in the Northern Hemisphere.</li>
+		</ul>
+		A number of days in between are also plotted.</p>
+
+		<p>Each hour of the day is plotted, showing how the sun's position changes over the course of a year. This is called an <a href="http://en.wikipedia.org/wiki/Analemma">analemma curve</a>.</p>
+
+		<p>Any position plotted in the first half of the year appears in <i>blue</i>, while any position in the second half appears in <i>green</i>. In addition, for timezones that use <a href="http://en.wikipedia.org/wiki/Daylight_saving_time">Daylight Saving Time</a> (DST), any position during daylight saving time is shown in a lighter shade of the same color.</p>
+		<img src="imgs/exp_copenhagen.png">
+		<p>In the case of Copenhagen we can see that summer days and winter days differ dramatically in the amount of sunlight (17 hours vs 7 hours at the most). Moreover, the sun reaches a much higher elevation in summer than it does in winter.</p>
+
+		<p>The selected point (the red dot) is the solar noon for that particular day (which happens to be March 13).</p>
 	</div>
 
 </body>
