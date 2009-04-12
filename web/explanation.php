eb6ef3548626b62a4d16923fbcbedfe4d3f06997
Martin Matusiak <numerodix@gmail.com>
2009-04-12 15:12:52 +0200

    web

--- web/explanation.php
+++ web/explanation.php
@@ -6,17 +6,17 @@
 </head>
 <body>
 
-	<? include("header.php"); ?>
+	<? //include("header.php"); ?>
 
 	<? include("menu.php"); ?>
 
 	<div class="content">
 		<h1>Explanation</h1>
 
-		A solar diagram shows the position (and in general, the trajectory) of the sun at a given time of day, for a given day of the year. It answers the questions:
+		A solar diagram shows the position (and in general, the trajectory) of the <a href="http://en.wikipedia.org/wiki/The_sun">sun</a> at a given time of day, for a given day of the year. It answers the questions:
 		<ul>
 			<li>Where is the sun in the sky?</li>
-			<li>How high in sky is the sun at noon?</li>
+			<li>How high in the sky is the sun at noon?</li>
 			<li>When does the sun rise and set?</li>
 			<li>How many hours of sunlight are there in January?</li>
 			<li>How different (in terms of sunlight) is a day in summer from a day in winter?</li>
