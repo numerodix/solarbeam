691bb839a4ab9d97bda0772f80912754d1c2ed3a
Martin Matusiak <numerodix@gmail.com>
2009-04-15 23:36:58 +0200

    web

---
+++ web/download.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<link type="text/css" href="styles.css" rel="stylesheet"/>
+	<link rel="icon" type="image/png" href="imgs/favicon.png"/>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<title>SolarBeam : Download</title>
+</head>
+<body>
+
+	<? include("header.php"); ?>
+
+	<? include("menu.php"); ?>
+
+	<div class="content">
+		<h1>Download</h1>
+
+		<h2>System requirements</h2>
+		<p>SolarBeam needs .NET 2.0+ or Mono 2.0+ to run.</p>
+
+		<p>On a Windows system, .NET is most likely already installed (comes preinstalled on Windows Vista), but if not you can download it <a href="http://www.microsoft.com/NET/">here</a>.</p>
+
+		<p>If you are not running Windows (or if you prefer Mono), Mono runs on every major operating system. You can download it <a href="http://www.go-mono.com/mono-downloads/download.html">here</a>.</p>
+	</div>
+
+</body>
+</html>
