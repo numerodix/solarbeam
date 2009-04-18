fb654921927a542c8fe356617e5b5dff958260de
Martin Matusiak <numerodix@gmail.com>
2009-04-18 13:51:08 +0200

    web

--- web/releases.php
+++ web/releases.php
@@ -1,16 +1,17 @@
-		<? // generated with scripts/genreleasepage.py on 17.4.2009 ?>
+		<? // generated with scripts/genreleasepage.py on 18.4.2009 ?>
 
 		<div class="releasetitle">
 			<a href="javascript:Collapse('release-0.5')">
 				<img src="imgs/collapse.png" alt="collapse"/>
 				SolarBeam 0.5
-			</a> - 17.04.2009
+			</a> - 18.04.2009
 		</div>
 		<div id="release-0.5">
 			<div class="releasecontent">
 				<p>New features:</p>
 				<ul>
 					<li>Dawn and dusk displays.</li>
+					<li>Caption under diagram.</li>
 				</ul>
 				<p class="download">
 					<a href="http://downloads.sourceforge.net/solarbeam/solarbeam-0.5.zip">
