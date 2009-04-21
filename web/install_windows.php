db3f87f47c27ac6f430578f7647a279497ffe1f5
Martin Matusiak <numerodix@gmail.com>
2009-04-22 01:10:34 +0200

    web

---
+++ web/install_windows.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<script type="text/javascript" src="scripts.js"></script>
+	<link type="text/css" href="styles.css" rel="stylesheet"/>
+	<link rel="icon" type="image/png" href="imgs/favicon.png"/>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<title>SolarBeam : Installation on Windows</title>
+</head>
+<body>
+
+	<? include("header.php"); ?>
+
+	<? include("menu.php"); ?>
+
+	<div class="content">
+		<h1>Installation on Windows</h1>
+		SolarBeam does not need to be installed in the conventional sense. All it takes to run it is to extract the .zip file and run the program. SolarBeam can also set up shortcuts for itself on the Desktop and in the Start Menu.
+
+		<h2>Extracting</h2>
+		<p>Right click on the .zip file and choose <i>Extract All...</i>.</p>
+		<img src="imgs/guide/win_1.png" alt="win_1"/>
+	
+		<p>Complete the wizard to extract the .zip file.</p>
+		<img src="imgs/guide/win_2.png" alt="win_2"/>
+		<img src="imgs/guide/win_3.png" alt="win_3"/>
+		<img src="imgs/guide/win_4.png" alt="win_4"/>
+	
+		<p>The extracted directory contains the program. You can put it anywhere on your system.</p>
+		<img src="imgs/guide/win_5.png" alt="win_5"/>
+
+		<p>Inside the directory you'll find the program. <i>solarbeam.exe</i> is the main executable. Run it!</p>
+		<img src="imgs/guide/win_6.png" alt="win_6"/>
+
+		<h2>Creating shortcuts</h2>
+		<p>Once you've started the program you can create shortcuts to SolarBeam on the Desktop and in the Start Menu by choosing <i>Help &gt; Create shortcuts...</i>.</p>
+		<img src="imgs/guide/win_7.png" alt="win_7"/>
+
+		<p>Click <i>Create</i>.</p>
+		<img src="imgs/guide/win_8.png" alt="win_8"/>
+
+		<p>You can now launch SolarBeam from the Desktop or Start Menu.</p>
+		<img src="imgs/guide/win_9.png" alt="win_9"/>
+	</div>
+
+</body>
+</html>
