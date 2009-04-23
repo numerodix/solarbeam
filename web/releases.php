32fa514ac3cbada8c0184469d18ebe0b9bf0aa9a
Martin Matusiak <numerodix@gmail.com>
2009-04-23 11:52:04 +0200

    web

--- web/releases.php
+++ web/releases.php
@@ -1,12 +1,34 @@
-		<? // generated with scripts/genreleasepage.py on 21.4.2009 ?>
+		<? // generated with scripts/genreleasepage.py on 23.4.2009 ?>
 
+		<div class="releasetitle">
+			<a href="javascript:Collapse('release-0.7')">
+				<img src="imgs/collapse.png" alt="collapse"/>
+				SolarBeam 0.7
+			</a> - 23.04.2009
+		</div>
+		<div id="release-0.7">
+			<div class="releasecontent">
+				<p>New features:</p>
+				<ul>
+					<li>Create shortcuts window.</li>
+					<li>Add help menu item.</li>
+					<li>Add toggle for sun trajectory rendering in save to diagram.</li>
+				</ul>
+				<p class="download">
+					<a href="http://downloads.sourceforge.net/solarbeam/solarbeam-0.7.zip">
+						<img src="imgs/download.png" alt="download"/>
+						Download
+					</a>
+				</p>
+			</div>
+		</div>
 		<div class="releasetitle">
 			<a href="javascript:Collapse('release-0.6')">
 				<img src="imgs/collapse.png" alt="collapse"/>
 				SolarBeam 0.6
 			</a> - 21.04.2009
 		</div>
-		<div id="release-0.6">
+		<div id="release-0.6" style="display: none;">
 			<div class="releasecontent">
 				<p>New features:</p>
 				<ul>
